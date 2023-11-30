cfe43241bacd798716e70816df3764ba7e9a0470
TanishaHirpara <150216881+TanishaHirpara@users.noreply.github.com>
2023-11-30 11:46:20 +0530

    Add files via upload

---
+++ ternary operator3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main()
+
+{
+	int a,b,c;
+	printf("Enter value of A");
+	scanf("%d" ,&a);
+	printf("Enter value of B");
+	scanf("%d" ,&b);
+	printf("Enter value of C");
+	scanf("%d" ,&c);
+	
+	(a>b) ? (a>c) ? printf("A is max") : printf("C is max")
+          : (b>c) ? printf("B is max") : printf("C is max");
+}
+
